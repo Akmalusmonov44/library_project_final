a3e1befd4d22f6d93d580a898c5f645f4c269870
Akmal Usmonov <154730050+Akmalusmonov44@users.noreply.github.com>
2025-05-03 19:59:46 +0500

    gunicorn o'rnatildi

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import EMAIL_BACKEND
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#_mv&ik6ltk&@h2l*4rrj9np$^h_nm$$^9fm(or!%=c67ck^yb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -146,6 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
